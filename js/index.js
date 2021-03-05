73d34e92e08efc4a8e2b9096e7a144357e2d5f34
Dawid Spisak <dawidspisak@gmail.com>
2021-03-05 15:41:11 +0100

    add hamburger layout

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const car = document.querySelector('.car-wrapper')
+    const carMenu = document.querySelector('.car-menu')
+    var clientHeight = document.querySelector('.car-wrapper').clientHeight;
+    console.log(clientHeight);
+
+})
