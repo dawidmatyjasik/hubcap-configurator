aa86980849cd1ae11041e6157f4b035dcbc76402
Dawid Spisak <dawidspisak@gmail.com>
2021-03-09 20:36:14 +0100

    add working rim menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+console.log("test");
+
 const handleMenu = () => {
     const list = document.querySelectorAll('.settings li');
     const carModelList = document.querySelector('.car-menu__model-list');
@@ -79,9 +81,7 @@ const handleCarMenu = () => {
             'car--size-van',
             'car--size-bus',
             'car--size-truck')
-
     }
-
     const pickModelAndColor = (e) => {
         const origin = e.target;
         if (origin.dataset.colorType || origin.dataset.carType) {
@@ -89,38 +89,49 @@ const handleCarMenu = () => {
             choices.colorChoice = origin.dataset.colorType || choices.colorChoice;
             carImg.src = `assets/auto/${choices.carChoice}/${choices.colorChoice}.png`
         }
-        resetCarValues()
+
         if (origin.dataset.carType === "bmw") {
+            resetCarValues()
             car.classList.add('car--size-default')
         }
         else if (origin.dataset.carType === "mazda") {
+            resetCarValues()
             car.classList.add('car--size-default')
         }
         else if (origin.dataset.carType === "mazda_wagon") {
+            resetCarValues()
             car.classList.add('car--size-default')
         }
         else if (origin.dataset.carType === "mercedes") {
+            resetCarValues()
             car.classList.add('car--size-mercedes')
         }
         else if (origin.dataset.carType === "range") {
+            resetCarValues()
             car.classList.add('car--size-range')
         }
         else if (origin.dataset.carType === "vw") {
+            resetCarValues()
             car.classList.add('car--size-vw')
         }
         else if (origin.dataset.carType === "fiat") {
+            resetCarValues()
             car.classList.add('car--size-fiat')
         }
         else if (origin.dataset.carType === "van") {
+            resetCarValues()
             car.classList.add('car--size-van')
         }
         else if (origin.dataset.carType === "minibus") {
+            resetCarValues()
             car.classList.add('car--size-van')
         }
         else if (origin.dataset.carType === "bus_scania") {
+            resetCarValues()
             car.classList.add('car--size-bus')
         }
         else if (origin.dataset.carType === "truck") {
+            resetCarValues()
             car.classList.add('car--size-truck')
         }
     }
@@ -136,6 +147,7 @@ const handleRimMenu = () => {
     const racing = document.querySelector('.racing');
     const jtac = document.querySelector('.jtac');
     const max6 = document.querySelector('.max6');
+    const logo = document.querySelector('.info__logo');
     const resetCompanyValues = () => {
         menu.forEach(li => {
             li.classList.add('is-hidden')
@@ -144,19 +156,81 @@ const handleRimMenu = () => {
     }
     const pickCompany = (e) => {
         const origin = e.target;
-        console.log(origin.dataset.companyName);
-        resetCompanyValues()
+        const b = new Splide('#b', {
+            perPage: 5,
+            perMove: 1,
+            breakpoints: {
+                500: {
+                    perPage: 2,
+                },
+                640: {
+                    perPage: 3,
+                },
+                780: {
+                    perPage: 3,
+                }
+            }
+        })
+        const c = new Splide('#c', {
+            perPage: 5,
+            perMove: 1,
+            breakpoints: {
+                500: {
+                    perPage: 2,
+                },
+                640: {
+                    perPage: 3,
+                },
+                780: {
+                    perPage: 3,
+                }
+            }
+        })
+        const d = new Splide('#d', {
+            perPage: 5,
+            perMove: 1,
+            breakpoints: {
+                500: {
+                    perPage: 2,
+                },
+                640: {
+                    perPage: 3,
+                },
+                780: {
+                    perPage: 3,
+                }
+            }
+        })
         if (origin.dataset.companyName === "argo") {
             argo.classList.remove('is-hidden')
+            racing.classList.add('is-hidden')
+            jtac.classList.add('is-hidden')
+            max6.classList.add('is-hidden')
+            logo.src = "assets/logo/argo.png"
         }
         else if (origin.dataset.companyName === "4racing") {
+            argo.classList.add('is-hidden')
             racing.classList.remove('is-hidden')
+            jtac.classList.add('is-hidden')
+            max6.classList.add('is-hidden')
+            logo.src = "assets/logo/4racing.png"
+            b.mount();
         }
         else if (origin.dataset.companyName === "jtac") {
+            argo.classList.add('is-hidden')
+            racing.classList.add('is-hidden')
             jtac.classList.remove('is-hidden')
+            max6.classList.add('is-hidden')
+            logo.src = "assets/logo/j-tec.png"
+            c.mount();
         }
         else if (origin.dataset.companyName === "max6") {
+            argo.classList.add('is-hidden')
+            racing.classList.add('is-hidden')
+            jtac.classList.add('is-hidden')
             max6.classList.remove('is-hidden')
+            logo.src = "assets/logo/max6.png"
+            d.mount();
         }
 
     }
@@ -164,15 +238,51 @@ const handleRimMenu = () => {
         li.addEventListener('click', pickCompany)
     })
 }
+const handleRimChange = () => {
+    const hubcaps = document.querySelectorAll('.car__hubcap')
+    const list = document.querySelectorAll('.navigation__photo')
+    const menu = document.querySelectorAll('.splide__image')
+
+    let src = {
+        company: `argo`,
+        model: `avalone_pro`
+    }
+    console.log(menu);
+
+    const pickCompany = (e) => {
+        const origin = e.target;
+        src.company = origin.dataset.companyName;
+        console.log(src);
+    }
+    const pickModel = (e) => {
+        const origin = e.target;
+        src.model = origin.dataset.modelName;
+        hubcaps.forEach(hubcap => {
+            hubcap.style.backgroundImage = `url('assets/kolpaki_bez_opon/${src.company}/${src.model}.png')`
+        })
+
+    }
+    list.forEach(li => {
+        li.addEventListener('click', pickCompany)
+    })
+    menu.forEach(li => {
+        li.addEventListener('click', pickModel)
+    })
+}
 
 
 
 
 
-handleRimMenu()
-handleCarMenu()
-handleWindowSize()
+
 handleMenu()
+handleWindowSize()
+handleCarMenu()
+handleRimMenu()
+handleRimChange()
+
+
+
 
 
 
