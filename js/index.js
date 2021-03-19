d44b0bf0e07bbeefe755c5728ff7769af92d176c
Dawid Spisak <dawidspisak@gmail.com>
2021-03-19 14:19:17 +0100

    add preloader

--- js/index.js
+++ js/index.js
@@ -537,6 +537,15 @@ const handleBackgroundChange = () => {
         }
     });
 } */
+const preloader = () => {
+    const preload = document.querySelector('.preload');
+
+    preload.classList.add('show-preloader');
+    window.addEventListener('load', () => {
+        preload.classList.remove('show-preloader');
+    });
+}
+preloader()
 renderUsers().then(r => {
     handleRimChange()
 });
